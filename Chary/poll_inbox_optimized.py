16cc27f9d56775bf2be9d48153f1cf5f524eaed0
tvbbchary <tvbbchary@gmail.com>
2021-04-20 16:54:07 +0530

    latest mailbox attributes feching task, & poll the inbox task files

---
+++ Chary/poll_inbox_optimized.py
@@ -0,0 +1,32 @@
+from reading_mails_fixed_review_comments import ReadMail, outlook
+import pyttsx3
+import time
+
+
+class PollInbox(ReadMail):
+
+    def poll_mail(self, inbox_folder):
+        outlook_obj = ReadMail()
+        inbox_count = inbox_folder.Items.Count
+        if inbox_count:
+            outlook_obj.inbox_mail(inbox_folder)
+            count =0
+            while(True):
+                count = count + 1
+                time.sleep(5)
+                if inbox_count < inbox_folder.Items.Count:
+                    engine = pyttsx3.init()
+                    engine.say("you received a new mail")
+                    engine.runAndWait()
+                    outlook_obj.inbox_mail(inbox_folder)
+                    inbox_count = inbox_folder.Items.Count
+                if count == 20:
+                    break
+        else:
+            print("No mails in Inbox")
+
+
+poll_obj = PollInbox()
+
+inbox_folder = outlook.GetDefaultFolder(6)
+poll_obj.poll_mail(inbox_folder)
