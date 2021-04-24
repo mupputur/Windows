46de383ed32f9c2b431e920fc4029a5003b98926
tvbbchary <tvbbchary@gmail.com>
2021-04-24 16:25:14 +0530

    Based on the review comments , i fixed the issues

--- Chary/reading_mails_fixed_review_comments.py
+++ Chary/reading_mails_fixed_review_comments.py
@@ -5,57 +5,36 @@ outlook = client.Dispatch("Outlook.Application").GetNameSpace("MAPI")
 
 class ReadMail:
 
-    def inbox_mail(self,folder):
-        self.inbox_message = folder.Items.GetLast()
-        mail_address = f"In Inbox : {self.message_mail_address(self.inbox_message)}"
-        time = f"The mail received at: {self.message_time(self.inbox_message)}"
-        sender = f"Sender name is: {self.message_sendger(self.inbox_message)}"
-        subject = f"subject in mail is: {self.message_subject(self.inbox_message)}"
-        body = f"Body of the Mail is: {self.message_body(self.inbox_message)}"
-        inbox_latest_message = [mail_address, time, sender, subject, body]
-        print(inbox_latest_message)
-        return inbox_latest_message
+    def inbox_mail(self, folder):
+        inbox_message = folder.Items.GetLast()
+        inbox_latest_megs = self.common(inbox_message)
+        return f"Inbox latest mail is:{inbox_latest_megs}"
 
-    def sent_mail(self):
-        sentbox_message = sentbox_folder.Items.GetLast()
-        mail_address = f"In sent box : {self.message_mail_address(sentbox_message)}"
-        time = f"time_stamp is : {self.message_time(sentbox_message)}"
-        sender = f"Sender Name is: {self.message_sendger(sentbox_message)}"
-        subject = f"subject is: {self.message_subject(sentbox_message)}"
-        body = f"Body of the sent Mail is: {self.message_body(sentbox_message)}"
-        sentbox_latest_message = [f"{mail_address}", time, sender, subject, body]
-        print(sentbox_latest_message)
-        return sentbox_latest_message
+    def sent_mail(self, folder):
+        sentbox_message = folder.Items.GetLast()
+        return f"Sentbox latest mail is: {self.common(sentbox_message)}"
 
-    def drafts_mail(self):
-        drafts_message = drafts_folder.Items.GetLast()
-        mail_address = f"In draftbox : {self.message_mail_address(drafts_message)}"
-        time = f"time_stamp is : {self.message_time(drafts_message)}"
-        subject = f"subject is: {self.message_subject(drafts_message)}"
-        body = f"Body of the draft Mail is: {self.message_body(drafts_message)}"
-        draftbox_latest_message = [mail_address, time, subject, body]
-        print(draftbox_latest_message)
-        return draftbox_latest_message
+    def drafts_mail(self, folder):
+        drafts_message = folder.Items.GetLast()
+        return f"Draftbox latest mail is: {self.common(drafts_message)}"
 
-    def deleted_mail(self):
-        deleted_message = deleted.Items.GetLast()
-        mail_address = f"In deleted box : {self.message_mail_address(deleted_message)}"
-        time = f" time stamp is : {self.message_time(deleted_message)}"
-        subject = f" Subject is : {self.message_subject(deleted_message)}"
-        body = f" Body of the deleted mail is : {self.message_body(deleted_message)}"
-        deleted_latest_message = [mail_address, time, subject, body]
-        print(deleted_latest_message)
-        return deleted_latest_message
+    def deleted_mail(self, folder):
+        deleted_message = folder.Items.GetLast()
+        return f"Deleated Box latest mail is: {self.common(deleted_message)}"
 
-    def outbox_mail(self):
-        outbox_message = outbox.Items.GetLast()
-        mail_address = f" In outbox : {self.message_mail_address(outbox_message)}"
-        time = f"time stamp is: {self.message_time(outbox_message)}"
-        subject = f"subject is : {self.message_subject(outbox_message)}"
-        body = f" Body of the outbox mail is : {self.message_body(outbox_message)}"
-        outbox_latest_message = [mail_address, time, subject, body]
-        print(outbox_latest_message)
-        return outbox_latest_message
+    def outbox_mail(self, folder):
+        outbox_message = folder.Items.GetLast()
+        return f"Outbox latest mail is: {self.common(outbox_message)}"
+
+    def common(self, attribute):
+        mail_address = self.message_mail_address(attribute)
+        time = f"The mail received at: {self.message_time(attribute)}"
+        sender = f"Sender name is: {self.message_sendger(attribute)}"
+        subject = f"subject in mail is: {self.message_subject(attribute)}"
+        body = f"Body of the Mail is: {self.message_body(attribute)}"
+        attchments = self.mail_attachments(attribute)
+        latest_message = [mail_address, time, sender, subject, body, attchments]
+        return latest_message
 
     def message_mail_address(self, folder):
         mail_ids1 = []
@@ -69,6 +48,22 @@ class ReadMail:
         except:
             return mail_ids
 
+    def mail_attachments(self, folder):
+        subFolderItemAttachments = folder.Attachments
+        no_of_attchments = subFolderItemAttachments.Count
+        if no_of_attchments:
+
+            attchments_list = [str(subFolderItemAttachments.item(x)) for x in range(1, (no_of_attchments+1)) ]
+            """attchments_list = []
+            for x in range(1, (no_of_attchments + 1)):
+                attch = subFolderItemAttachments.item(x)
+                attchments_list.append(str(attch))"""
+            return f" mail having the attachments {attchments_list}"
+        else:
+            return " No attachments in the mail"
+
+
+
     def message_sendger(self, folder):
         return folder.SenderName
 
@@ -79,6 +74,7 @@ class ReadMail:
         return folder.Body
 
     def message_time(self, folder):
+
         return folder.ReceivedTime
 
 
@@ -87,37 +83,35 @@ outlook_obj = ReadMail()
 
 
 def folder_acces(name):
-    try:
-        folder = str(name)
-        count = name.Items.Count
-        if count == 0:
-            print(f"No mails in {name}")
-        if folder == "Inbox":
-            inbox_result = outlook_obj.inbox_mail(name)
-            return inbox_result
-        elif folder == 'Sent Items':
-            sent_box_result = outlook_obj.sent_mail()
-            return sent_box_result
-        elif folder == 'Drafts':
-            drafts_result = outlook_obj.drafts_mail()
-            return drafts_result
-        elif folder == 'Deleted Items':
-            deleted_result = outlook_obj.deleted_mail()
-            return deleted_result
-        elif folder == 'outbox':
-            outbox_result = outlook_obj.outbox_mail()
-            return outbox_result
-    except:
-        return False
+    folder = str(name)
+    count = name.Items.Count
+    if count == 0:
+        return f"No mails in {name}"
+    if folder == "Inbox":
+        inbox_result = outlook_obj.inbox_mail(name)
+        return inbox_result
+    elif folder == 'Sent Items':
+        sent_box_result = outlook_obj.sent_mail(name)
+        return sent_box_result
+    elif folder == 'Drafts':
+        drafts_result = outlook_obj.drafts_mail(name)
+        return drafts_result
+    elif folder == 'Deleted Items':
+        deleted_result = outlook_obj.deleted_mail(name)
+        return deleted_result
+    elif folder == 'outbox':
+        outbox_result = outlook_obj.outbox_mail(name)
+        return outbox_result
+
 
 if __name__ == "__main__":
     inbox_folder = outlook.GetDefaultFolder(6)
-    folder_acces(inbox_folder)
+    print(folder_acces(inbox_folder))
     sentbox_folder = outlook.GetDefaultFolder(5)
-    folder_acces(sentbox_folder)
+    print(folder_acces(sentbox_folder))
     drafts_folder = outlook.GetDefaultFolder(16)
-    folder_acces(drafts_folder)
+    print(folder_acces(drafts_folder))
     deleted = outlook.GetDefaultFolder(3)
-    folder_acces(deleted)
+    print(folder_acces(deleted))
     outbox = outlook.GetDefaultFolder(4)
-    folder_acces(outbox)
+    print(folder_acces(outbox))
